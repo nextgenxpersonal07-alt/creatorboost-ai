5f1f884d8ba5a442ba59fa42183bfa54f77b5213
nextgenxpersonal07-alt <nextgenx.personal07@gmail.com>
2026-06-02 04:11:58 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="i-JprjrYos97SYakX0mU38D7foE9izCn0ODnPLLUtK8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@500;600;700;800&display=swap" rel="stylesheet" />
