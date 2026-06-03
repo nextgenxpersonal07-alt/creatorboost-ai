2a9acab364613a0596a4c139128db2420ccc153c
nextgenxpersonal07-alt <nextgenx.personal07@gmail.com>
2026-06-03 00:01:26 -0400

    Add Monetag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@500;600;700;800&display=swap" rel="stylesheet" />
+        <script src="https://quge5.com/88/tag.min.js" data-zone="245664" async data-cfasync="false"></script>
       </head>
       <body className="font-body antialiased bg-[#F7F8FF]">
         {children}
